63d49f72a0d0199ecaae0af729c8beef02748af0
Sephyros <leo037br@hotmail.com>
2021-06-16 18:21:53 -0300

    Resolution of the proposed exercises from list 12

--- exercicio-12/app.js
+++ exercicio-12/app.js
@@ -13,19 +13,30 @@
 */
 
 const best2019Movies = [
-  { title: 'Parasita', directedBy: 'Bong Joon-ho' },
-  { title: 'Bacurau', directedBy: 'Kleber Mendonça Filho' },
-  { title: 'O Irlandês', directedBy: 'Martin Scorsese' },
-  { title: 'Entre Facas e Segredos', directedBy: 'Rian Johnson' },
-  { title: 'Turma da Mônica: Laços', directedBy: 'Daniel Rezende' },
-  { title: 'Homem-Aranha: No Aranhaverso', directedBy: 'Peter Ramsey' },
-  { title: 'Klaus', directedBy: 'Sergio Pablos' },
-  { title: 'Fora de Série', directedBy: 'Olivia Wilde' },
-  { title: 'Shazam', directedBy: 'David F. Sandberg' },
-  { title: 'História de um Casamento', directedBy: 'Noah Baumbach' },
-  { title: 'Varda por Agnès', directedBy: 'Agnès Varda' },
-  { title: 'A Vida Invisível', directedBy: 'Karim Aïnouz' }
-]
+  { title: "Parasita", directedBy: "Bong Joon-ho" },
+  { title: "Bacurau", directedBy: "Kleber Mendonça Filho" },
+  { title: "O Irlandês", directedBy: "Martin Scorsese" },
+  { title: "Entre Facas e Segredos", directedBy: "Rian Johnson" },
+  { title: "Turma da Mônica: Laços", directedBy: "Daniel Rezende" },
+  { title: "Homem-Aranha: No Aranhaverso", directedBy: "Peter Ramsey" },
+  { title: "Klaus", directedBy: "Sergio Pablos" },
+  { title: "Fora de Série", directedBy: "Olivia Wilde" },
+  { title: "Shazam", directedBy: "David F. Sandberg" },
+  { title: "História de um Casamento", directedBy: "Noah Baumbach" },
+  { title: "Varda por Agnès", directedBy: "Agnès Varda" },
+  { title: "A Vida Invisível", directedBy: "Karim Aïnouz" },
+];
+
+const showMoviesInfo = (moviesArray) => {
+  let moviesInfo = "";
+  moviesArray.forEach((movie) => {
+    moviesInfo = `${moviesInfo}
+  - ${movie.title}, dirigido por ${movie.directedBy}`;
+  });
+  return `Segundo o site Omelete, os melhores filmes de 2019 são:${moviesInfo}`;
+};
+
+console.log(showMoviesInfo(best2019Movies));
 
 /*
   02
@@ -42,25 +53,38 @@ const best2019Movies = [
 */
 
 const youtubeUser = {
-  name: 'Roger Melo',
-  chanelUrl: 'https://www.youtube.com/channel/UCmjDevp9Y8r-qi-xueD3Izg',
+  name: "Roger Melo",
+  chanelUrl: "https://www.youtube.com/channel/UCmjDevp9Y8r-qi-xueD3Izg",
   videos: {
     total: 111,
     recentVideos: [
-      { title: 'Introdução ao TDD - Parte 02 | JavaScript | Jest', length: '28:28' },
-      { title: 'Introdução ao TDD | JavaScript | Jest', length: '19:29' },
-      { title: 'Higher-order Functions | JavaScript', length: '47:38' }
-    ]
+      {
+        title: "Introdução ao TDD - Parte 02 | JavaScript | Jest",
+        length: "28:28",
+      },
+      { title: "Introdução ao TDD | JavaScript | Jest", length: "19:29" },
+      { title: "Higher-order Functions | JavaScript", length: "47:38" },
+    ],
   },
   about: {
-    description: '🔥 Ensino as pessoas a alcançarem a fluência em JavaScript! 🔥',
+    description:
+      "🔥 Ensino as pessoas a alcançarem a fluência em JavaScript! 🔥",
     socialNetworks: {
-      twitter: 'https://twitter.com/rogermelodev',
-      gitHub: 'https://github.com/Roger-Melo'
+      twitter: "https://twitter.com/rogermelodev",
+      gitHub: "https://github.com/Roger-Melo",
     },
-    country: 'Brasil'
-  }
-}
+    country: "Brasil",
+  },
+  showRecentVideos() {
+    let recentVideosList = "";
+    this.videos.recentVideos.forEach((video) => {
+      recentVideosList += `${video.title}\n`;
+    });
+    return `Vídeos recentes de ${this.name}:\n${recentVideosList}`;
+  },
+};
+
+console.log(youtubeUser.showRecentVideos());
 
 /*
   03
@@ -68,7 +92,7 @@ const youtubeUser = {
   - Exiba o valor do PI no console.
 */
 
-
+console.log(Math.PI);
 
 /*
   04
@@ -77,7 +101,8 @@ const youtubeUser = {
     console.
 */
 
-const firstNumber = 8.3
+const firstNumber = 8.3;
+console.log(Math.ceil(firstNumber));
 
 /*
   05
@@ -86,7 +111,8 @@ const firstNumber = 8.3
     forma padrão, e exiba-o no console.
 */
 
-const secondNumber = 3.5
+const secondNumber = 3.5;
+console.log(Math.round(secondNumber));
 
 /*
   06
@@ -95,7 +121,8 @@ const secondNumber = 3.5
     console.
 */
 
-const thirdNumber = 8.9
+const thirdNumber = 8.9;
+console.log(Math.round(thirdNumber));
 
 /*
   07
@@ -103,7 +130,8 @@ const thirdNumber = 8.9
   - Exiba no console o número abaixo com a parte decimal removida.
 */
 
-const fourthNumber = 5.5
+const fourthNumber = 5.5;
+console.log(Math.trunc(fourthNumber));
 
 /*
   08
@@ -111,3 +139,5 @@ const fourthNumber = 5.5
   - A cada vez que o index.html for carregado, exiba no console um número  
     aleatório de 0 à 10, incluindo 0 e 10.
 */
+
+console.log(Math.round(Math.random() * 10));
