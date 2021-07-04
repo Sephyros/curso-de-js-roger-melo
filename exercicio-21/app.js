5f30d2358272c00d55215e0475fa470717d1fc68
Sephyros <leo037br@hotmail.com>
2021-07-04 16:54:15 -0300

    Resolution of the proposed exercises from list 21

--- exercicio-21/app.js
+++ exercicio-21/app.js
@@ -5,7 +5,9 @@
     o novo array no console.
 */
 
-const randomNumbers = [10, 30, 15, 25, 50, 40, 5]
+const randomNumbers = [10, 30, 15, 25, 50, 40, 5];
+const oddNumbers = randomNumbers.filter((number) => number % 2 !== 0);
+console.log(oddNumbers);
 
 /*
   02
@@ -13,7 +15,14 @@ const randomNumbers = [10, 30, 15, 25, 50, 40, 5]
   - Exiba no console quantos números abaixo de 501 o array abaixo possui.
 */
 
-const crazyNumbers = [937, 5, 395, 402, 501, 333, 502, 781, 3, 691]
+const crazyNumbers = [937, 5, 395, 402, 501, 333, 502, 781, 3, 691];
+const numbersBelow501 = crazyNumbers.reduce((accumulator, item) => {
+  if (item < 501) {
+    accumulator++;
+  }
+  return accumulator;
+}, 0);
+console.log(numbersBelow501);
 
 /*
   03
@@ -22,7 +31,9 @@ const crazyNumbers = [937, 5, 395, 402, 501, 333, 502, 781, 3, 691]
     exiba o novo array no console.
 */
 
-const numbers = [5, 7, 3]
+const numbers = [5, 7, 3];
+const numbersToPow2 = numbers.map((number) => number ** 2);
+console.log(numbersToPow2);
 
 /*
   04
@@ -36,18 +47,24 @@ const numbers = [5, 7, 3]
 */
 
 const cart = [
-  { name: 'Dark Souls III', price: 95.03 },
-  { name: 'Shadow of the Tomb Raider', price: 101.19 },
-  { name: 'Sekiro: Shadows Die Twice', price: 179.99 },
-  { name: 'Resident Evil 2', price: 119.90 },
-  { name: 'Death Stranding', price: 149.99 }
-]
+  { name: "Dark Souls III", price: 95.03 },
+  { name: "Shadow of the Tomb Raider", price: 101.19 },
+  { name: "Sekiro: Shadows Die Twice", price: 179.99 },
+  { name: "Resident Evil 2", price: 119.9 },
+  { name: "Death Stranding", price: 149.99 },
+];
 
 /*
   - Nome 1
   - Nome 2
   - Nome 3
 */
+
+const gamesNameList = cart.reduce((accumulator, cartItem) => {
+  accumulator += `- ${cartItem.name}\n`;
+  return accumulator;
+}, "");
+console.log(gamesNameList);
 
 /*
   05
@@ -58,17 +75,24 @@ const cart = [
 */
 
 const tarantinoMovies = [
-  { name: 'Bastardos inglórios', release: 2009 },
-  { name: 'Pulp Fiction', release: 1994 },
-  { name: 'Kill Bill: Volume 2', release: 2004 },
-  { name: 'Quatro Quartos', release: 1995 },
-  { name: 'Sin City', release: 2005 },
-  { name: 'Era uma Vez em... Hollywood', release: 2019 },
-  { name: 'Django Livre', release: 2012 },
-  { name: 'Cães de Aluguel', release: 1992 },
-  { name: 'À Prova de Morte', release: 2007 },
-  { name: 'Kill Bill: Volume 1', release: 2003 }
-]
+  { name: "Bastardos inglórios", release: 2009 },
+  { name: "Pulp Fiction", release: 1994 },
+  { name: "Kill Bill: Volume 2", release: 2004 },
+  { name: "Quatro Quartos", release: 1995 },
+  { name: "Sin City", release: 2005 },
+  { name: "Era uma Vez em... Hollywood", release: 2019 },
+  { name: "Django Livre", release: 2012 },
+  { name: "Cães de Aluguel", release: 1992 },
+  { name: "À Prova de Morte", release: 2007 },
+  { name: "Kill Bill: Volume 1", release: 2003 },
+];
+
+const moviesBefore2000 = tarantinoMovies.filter((movie) => {
+  if (movie.release < 2000) {
+    return movie;
+  }
+});
+console.log(moviesBefore2000);
 
 /*
   06
@@ -78,14 +102,20 @@ const tarantinoMovies = [
 */
 
 const tvShows = [
-  { name: 'Breaking Bad', releaseYear: 2008 },
-  { name: 'Mr. Robot', releaseYear: 2015 },
-  { name: 'True Detective', releaseYear: 2014 },
-  { name: 'Hannibal', releaseYear: 2013 },
-  { name: 'The Handmaid\'s Tale', releaseYear: 2017 },
-  { name: 'House M.D.', releaseYear: 2004 },
-  { name: 'Watchmen', releaseYear: 2019 }
-]
+  { name: "Breaking Bad", releaseYear: 2008 },
+  { name: "Mr. Robot", releaseYear: 2015 },
+  { name: "True Detective", releaseYear: 2014 },
+  { name: "Hannibal", releaseYear: 2013 },
+  { name: "The Handmaid's Tale", releaseYear: 2017 },
+  { name: "House M.D.", releaseYear: 2004 },
+  { name: "Watchmen", releaseYear: 2019 },
+];
+
+const tvShowsNames = tvShows.map((tvShow) => {
+  return tvShow.name;
+});
+
+console.log(tvShowsNames);
 
 /*
   07
