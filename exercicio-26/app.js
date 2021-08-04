abebebae44457834246c41f1af2de9c129d010bb
Sephyros <leo037br@hotmail.com>
2021-08-03 21:36:06 -0300

    Resolution of the proposed exercises from list 26

--- exercicio-26/app.js
+++ exercicio-26/app.js
@@ -5,6 +5,14 @@
     formatação "DD/MM/AAAA". Exemplo: 03/07/2021;
   - Não utilize a date-fns.
 */
+const myTestDate = new Date();
+const formatDate = (date) => {
+  const day = date.getDay() <= 9 ? "0" + date.getDay() : date.getDay();
+  const month = date.getMonth() <= 9 ? "0" + date.getMonth() : date.getMonth();
+  const year = date.getFullYear();
+  return `${day}/${month}/${year}`;
+};
+console.log(formatDate(myTestDate));
 
 /*
   02
@@ -14,6 +22,22 @@
   - Não utilize a date-fns.
 */
 
+const longStrangeFormat = (date) => {
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const day = date.getDay();
+  const month = new Intl.DateTimeFormat("pt-BR", { month: "long" }).format(
+    date.getMonth()
+  );
+  const year = date.getFullYear();
+  const time = `${hours <= 9 ? "0" + hours : hours}:${minutes}`;
+  const weekday = new Intl.DateTimeFormat("pt-BR", { weekday: "long" }).format(
+    date.getDay()
+  );
+  return `${time} - ${weekday}, ${day} de ${month} de ${year}`;
+};
+console.log(longStrangeFormat(myTestDate));
+
 /*
   03
 
@@ -22,7 +46,9 @@
   - Não modifique a declaração da const user.
 */
 
-const user = { id: 42, isVerified: true }
+const user = { id: 42, isVerified: true };
+const { id, isVerified } = user;
+console.log(id, isVerified);
 
 /*
   04
@@ -34,8 +60,11 @@ const user = { id: 42, isVerified: true }
   - Não modifique a declaração das consts "robotA" e "robotB".
 */
 
-const robotA = { name: 'Bender' }
-const robotB = { name: 'HAL 9000' }
+const robotA = { name: "Bender" };
+const robotB = { name: "HAL 9000" };
+const { name: nameA } = robotA;
+const { name: nameB } = robotB;
+console.log(nameA, nameB);
 
 /*
   05
@@ -46,9 +75,12 @@ const robotB = { name: 'HAL 9000' }
   - Exiba o objeto no console.
 */
 
-const a = 'a'
-const b = 'b'
-const c = 'c'
+const a = "a";
+const b = "b";
+const c = "c";
+
+const shorthandedObject = { a, b, c };
+console.log(shorthandedObject);
 
 /*
   06
@@ -56,27 +88,29 @@ const c = 'c'
   - Refatore o código abaixo.
 */
 
-const useDataSomewhereElse = value => {
-  console.log(value)
-}
+// Não consegui identificar o que deveria ser refatorado
+
+const useDataSomewhereElse = (value) => {
+  console.log(value);
+};
 
 const updateSomething = (data = {}) => {
-  const target = data.target
-  const property = data.property
-  let willChange = data.willChange
+  const target = data.target;
+  const property = data.property;
+  let willChange = data.willChange;
 
-  if (willChange === 'valor indesejado') {
-    willChange = 'valor desejado'
+  if (willChange === "valor indesejado") {
+    willChange = "valor desejado";
   }
 
   useDataSomewhereElse({
     target: target,
     property: property,
-    willChange: willChange
-  })
-}
+    willChange: willChange,
+  });
+};
 
-updateSomething({ target: '1', property: '2', willChange: 'valor indesejado' })
+updateSomething({ target: "1", property: "2", willChange: "valor indesejado" });
 
 /*
   07
@@ -85,21 +119,23 @@ updateSomething({ target: '1', property: '2', willChange: 'valor indesejado' })
     passada. Refatore-o.
 */
 
-const clockContainer = document.querySelector('.clock-container')
+// Não consegui identificar o que deveria ser refatorado
+
+const clockContainer = document.querySelector(".clock-container");
 
 const updateClock = () => {
-  const present = new Date()
-  const hours = present.getHours()
-  const minutes = present.getMinutes()
-  const seconds = present.getSeconds()
+  const present = new Date();
+  const hours = present.getHours();
+  const minutes = present.getMinutes();
+  const seconds = present.getSeconds();
 
   const clockHTML = `
     <span>${String(hours).length === 1 ? `0${hours}` : hours}</span> :
     <span>${String(minutes).length === 1 ? `0${minutes}` : minutes}</span> :
     <span>${String(seconds).length === 1 ? `0${seconds}` : seconds}</span>
-  `
+  `;
 
-  clockContainer.innerHTML = clockHTML
-}
+  clockContainer.innerHTML = clockHTML;
+};
 
-setInterval(updateClock, 1000)
+setInterval(updateClock, 1000);
